849138619c34e397f4f4b5aa0354e30de747b9ac
Kartikay2003 <kartikay252003@gmail.com>
2026-02-25 01:36:57 +0530

    implement header

---
+++ app/{root}/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button";
+
+const Home = () => {
+    return (
+        <div className="flex min-h-screen home-wrapper">
+            {// ctrl + click on custom class -> to see what it does (will open that class in CSS)
+            }
+            Home
+        </div>
+    )
+}
+export default Home
