4c37424dd6305301957915806555636567bcab5c
Kartikay2003 <kartikay252003@gmail.com>
2026-03-03 04:45:36 +0530

    implement auth logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,19 +3,19 @@ import Image from "next/image";
 import NavItems from "@/components/NavItems";
 import UserDropdown from "@/components/UserDropdown";
 
-const Header = () => {
+const Header = ({ user }: { user: User }) => {
     return (
         <header className="sticky top-0 header">
             <div className="container header-wrapper">
                 <Link href="/">
-                    <Image src="/assets/icons/logo.svg" alt="Stock Sense logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
+                    <Image src="/assets/icons/Stock_Sense_logo.png" alt="Stock Sense logo" width={140} height={32} className="h-14 w-auto max-w-50 cursor-pointer" />
                     {/* cursor-pointer tailwind class - so its clickable */}
                 </Link>
                 <nav className="hidden sm:block">
                     {/* sm - small devices */}
                     <NavItems />
                 </nav>
-                <UserDropdown />
+                <UserDropdown user={user} />
                 {/* creating components for cleaner code & DRY concept */}
             </div>
         </header>
